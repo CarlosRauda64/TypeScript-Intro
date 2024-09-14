dadb80c1a50485d01a4efa8a7444dc274d216c62
CarlosRauda64 <rauda.carlos6432@gmail.com>
2024-09-14 17:29:26 -0600

    :sparkles:Añadiendo interfaces a la API

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,17 @@
-interface IUsuario {
-  nombre: string;
-  apellido: string;
-  animalFav: string[];
-  edad: number;
-  vivo: boolean;
-  estatura: number;
+import axios from "axios"
+
+
+
+const getCharacters = async () => {
+  try {
+    const response = await axios.get('https://rickandmortyapi.com/api/character')
+    console.log(response.data)
+  }
+  catch (error) {
+    throw new Error('Error API')
+  }
 }
 
-let usuario: IUsuario = {
-  nombre: 'Carlos Rodrigo',
-  apellido: 'Portillo',
-  animalFav: ['Perros', 'Gatos', 'Leones'],
-  edad: 23,
-  vivo: true,
-  estatura: 2.00
-}
 
-console.log(usuario.apellido)
+getCharacters()
 
-enum Genero {
-  Femenino = 'Femenino', 
-  Masculino = 'Masculino'
-}
-
-type Alumno = {
-  nombre: string;
-  genero: Genero;
-  edad: number;
-  activo: boolean;
-}
-
-let alumno:Alumno = {
-  nombre: 'Carlos Rauda',
-  genero: Genero.Masculino,
-  edad: 21,
-  activo: true
-}
-
-console.log(alumno.genero)
