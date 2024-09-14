2d47acffdbeda6c1aedb07bad9fe4aba6724fce6
CarlosRauda64 <rauda.carlos6432@gmail.com>
2024-09-14 15:07:53 -0600

    :sparkles:Add intro to TypeScript

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+
+let fullname="John Doe"
+let address:string = "El Salvador, San Salvador"
+let edad:number = 25
+let alive:boolean = true
+let asistencia = false
+
+let today:Date = new Date()
+
+let cualquierCosa:any = "Hola"
+
+console.log(fullname)
+console.log(address)
+console.log(edad)
+console.log(today)
